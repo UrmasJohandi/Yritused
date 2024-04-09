cdfb9b3b95500512c67451bbe90dc29a0c26bffa
Urmas Johandi <urmas.johandi@gmail.com>
2024-04-09 14:52:13 +0300

    Commit 09.04.2024 14:51

---
+++ Yritused/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Yritused.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Yritus> Yritused => Set<Yritus>();
+        public DbSet<Osavotja> Osavotjad => Set<Osavotja>();
+        public DbSet<YritusOsavotja> YritusOsavotjad => Set<YritusOsavotja>();
+    }
+}
--- Yritused/Program.cs
+++ Yritused/Program.cs
@@ -1,10 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Yritused.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStrings:YritusedConnection"]); });
 
 var app = builder.Build();
-
-// app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
