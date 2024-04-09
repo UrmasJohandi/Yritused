237900629d8365f0b21195aecb9b0f05e9c40da2
Urmas Johandi <urmas.johandi@gmail.com>
2024-04-09 11:54:11 +0300

    09.04.2024

--- Yritused/obj/Debug/net8.0/Yritused.AssemblyInfo.cs
+++ Yritused/obj/Debug/net8.0/Yritused.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yritused")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0874c9e177ddd23c4ab0dbe389899f47fcd51e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yritused")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yritused")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
